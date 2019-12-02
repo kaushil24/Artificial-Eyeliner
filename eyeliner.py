86c010f70fb709dcea0ff5220af7e46e576bc334
kausil24 <kaushil.kundalia@gmai.com>
2019-12-02 23:17:21 +0530

    Minor modifications

--- eyeliner.py
+++ eyeliner.py
@@ -7,6 +7,8 @@ from scipy.interpolate import interp1d
 from imutils import face_utils
 import argparse
 import os
+from PIL import Image
+
 
 
 def getEyeLandnarkPts(face_landmark_points):
@@ -55,13 +57,14 @@ def getEyelinerPoints(eye_landmark_points):
     return [(L_interp_x, L_interp_top_y, L_interp_bottom_y), (R_interp_x, R_interp_top_y, R_interp_bottom_y)]
 
 
-def drawEyeliner(img, interp_pts, thickness = 0.5):
+def drawEyeliner(img, interp_pts, color = (85,90,92) , thickness = 1):
     L_eye_interp, R_eye_interp = interp_pts
 
     L_interp_x, L_interp_top_y, L_interp_bottom_y = L_eye_interp
     R_interp_x, R_interp_top_y, R_interp_bottom_y = R_eye_interp
 
-    overlay = np.empty(img.shape)
+    overlay = img.copy()
+    # overlay = np.empty(img.shape)
     # overlay = np.zeros_like(img)
 
     for i in range(len(L_interp_x)-2):
@@ -69,11 +72,11 @@ def drawEyeliner(img, interp_pts, thickness = 0.5):
         y1_top = L_interp_top_y[i]
         x2 = L_interp_x[i+1]
         y2_top = L_interp_top_y[i+1]
-        cv2.line(overlay, (x1, y1_top), (x2, y2_top), (0,0,0), thickness)
+        cv2.line(overlay, (x1, y1_top), (x2, y2_top), color, thickness)
 
         y1_bottom = L_interp_bottom_y[i]
         y2_bottom = L_interp_bottom_y[i+1]
-        cv2.line(overlay, (x1, y1_bottom), (x1, y2_bottom), (0,0,0), thickness)
+        cv2.line(overlay, (x1, y1_bottom), (x1, y2_bottom), color, thickness)
 
     
     for i in range(len(R_interp_x)-2):
@@ -81,15 +84,20 @@ def drawEyeliner(img, interp_pts, thickness = 0.5):
         y1_top = R_interp_top_y[i]
         x2 = R_interp_x[i+1]
         y2_top = R_interp_top_y[i+1]
-        cv2.line(img, (x1, y1_top), (x2, y2_top), (0,0,0), thickness)
+        cv2.line(overlay, (x1, y1_top), (x2, y2_top), color, thickness)
 
         y1_bottom = R_interp_bottom_y[i]
         y2_bottom = R_interp_bottom_y[i+1]
-        cv2.line(img, (x1, y1_bottom), (x1, y2_bottom), (0,0,0), thickness)
+        cv2.line(overlay, (x1, y1_bottom), (x1, y2_bottom), color, thickness)
 
-    img = cv2.addWeighted(overlay, 0.5, img, 0.5, 0, dtype = cv2.CV_32F)
+    # background = Image.fromarray(img) # .convert("1")
+    # foreground = Image.fromarray(overlay).convert("1")
 
-    return img
+    # newImg = Image.composite(foreground, background, foreground)#, mask='1')
+    
+    # # img = cv2.bitwise_and(overlay, img)
+    # return cv2.cvtColor(np.array(newImg), cv2.COLOR_RGB2BGR)
+    return overlay
 
 
 
@@ -104,14 +112,14 @@ if __name__ == "__main__":
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         bounding_boxes = face_detector(gray, 0)# The 2nd argument means that we upscale the image by 'x' number of times to detect more faces.
         lndMrkDetector = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-        output_frame = frame.copy()
+        
 
         for i, bb in enumerate(bounding_boxes):
             face_landmark_points = lndMrkDetector(gray, bb)
             face_landmark_points = face_utils.shape_to_np(face_landmark_points)
             eye_landmark_points = getEyeLandnarkPts(face_landmark_points)
             eyeliner_points = getEyelinerPoints(eye_landmark_points)
-            output_frame = drawEyeliner(output_frame, eyeliner_points, 2) 
+            output_frame = drawEyeliner(frame, eyeliner_points,color=(0,0,0),thickness= 2) 
             
         
         
